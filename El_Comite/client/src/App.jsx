b6f11b295268d6f5a68c6c179cae2c188257e2a1
LuisafdaEM18 <lespinalmontoya@gmail.com>
2025-04-21 19:39:59 -0500

    se añadio conexion de la pagina de inicio de sesion

--- El_Comite/client/src/App.jsx
+++ El_Comite/client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../src/Pages/Home.jsx' 
+import IniciarSesion from '../src/Pages/IniciarSesion.jsx'
 import './App.css'
 
 
@@ -8,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/iniciar-sesion" element={<IniciarSesion/>} /> 
       </Routes>
     </Router>
   )
